22c48772252ff04a246a02913fe4fccb312fa831
santiago gonzalez vargas <sgonzalezvargas.7cc@gmail.com>
2020-03-06 11:58:10 -0500

    version 1

---
+++ ApiWeb/Controllers/OrderController.cs
@@ -0,0 +1,153 @@
+﻿using ApiWeb.Models;
+using ApiWeb.Rules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ApiWeb.Controllers
+{
+    public class OrderController : Controller
+    {
+
+        // GET: Order
+        public ActionResult Index()
+        {
+
+            try
+            {
+                xyzContext db = new xyzContext();
+
+                //List<tbl_order> lista =  db.tbl_order.ToList();
+
+                return View(db.tbl_order.ToList());
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            
+        }
+
+        #region "Add"
+        public ActionResult add() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult add(tbl_order order)
+        {
+            Rule rules = new Rule(order.day, order.request, order.typeVehicle, order.quantity);
+
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+
+                using (var db = new xyzContext())
+                {
+                    db.tbl_order.Add(order);            
+                    db.SaveChanges();                  
+                }
+
+                using (var db1 = new xyzContext())
+                {
+                    tbl_order order1 = new tbl_order();
+                    
+                    rules.order();
+                    order.day = rules.AssignedDay;
+                    order.quantity = rules.QuantityAvalible;
+                    order.request = rules.Request;
+                    order.typeVehicle = rules.TypeVehicle;
+
+                    order1 = order;
+                    db1.tbl_order.Add(order1);
+                
+                    db1.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("Error al crear", ex.Message);
+                return View();
+            }                                               
+        }
+
+        #endregion
+
+        #region "Edit"
+        public ActionResult edit(int id) 
+        {
+            try
+            {
+                using (var db = new xyzContext())
+                {
+                    //tbl_order order = db.tbl_order.Where(o => o.id == id).FirstOrDefault();
+                    tbl_order order1 = db.tbl_order.Find(id);
+                    return View(order1);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }           
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult edit( tbl_order order ) 
+        {
+            try
+            {
+                using (var db = new xyzContext())
+                {
+                    tbl_order order1 = db.tbl_order.Find(order.id);
+                    order1.day = order.day;
+                    order1.request = order.request;
+                    order1.typeVehicle = order.typeVehicle;
+                    order1.quantity = order.quantity;
+
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }                
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            
+        }
+
+        #endregion
+
+        #region "Delete"
+
+        public ActionResult delete(int id)
+        {
+            using (var db = new xyzContext())
+            {
+                tbl_order order1 = db.tbl_order.Find(id);
+                db.tbl_order.Remove(order1);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        #endregion
+
+
+
+
+
+    }
+}
---
+++ ApiWeb/Rules/Rule.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiWeb.Rules
+{
+    public partial class Rule
+    {
+
+        private string _day;
+        private string _request;
+        private string _typeVehicle;
+        private int _quantity;
+        
+
+        private int _checkTypeVehicle;
+        private int _occupation;
+        private string assignedDay;
+        private int quantityAvalible;
+        private int _checkQuantity;
+
+        #region "propiedades"
+        public string AssignedDay 
+        {
+            get { return  assignedDay; }
+        }
+
+        public int QuantityAvalible
+        {
+            get { return _quantity - quantityAvalible ; }
+        }
+
+        public string TypeVehicle
+        {
+            get { return _typeVehicle; }
+        }
+
+        public string Request
+        {
+            get { return _request; }
+        }
+
+        #endregion
+
+        public Rule(string day, string request, string typeVehicle, int quantity)
+        {
+            _day = day;
+            _request = request;
+            _typeVehicle = typeVehicle;
+            _quantity = quantity;
+        }
+
+        public void order()
+        {
+            //int high = 0;
+
+            //while (++high * _quantity > 16) 
+            //{
+            //    _checkTypeVehicle = chechkTypeVehicle(_typeVehicle); // trae el tipo de vehiculo
+            //    _occupation = _checkTypeVehicle * _quantity; // calcula el total de horas por orden creada
+            //    avalibleDay(_occupation, _day, _checkTypeVehicle); // calcula ocupacion y asigna dia siguiente si es necesario
+            //}
+
+            _checkTypeVehicle = chechkTypeVehicle(_typeVehicle); // trae el tipo de vehiculo
+            _occupation = _checkTypeVehicle * _quantity; // calcula el total de horas por orden creada
+            avalibleDay(_occupation, _day, _checkTypeVehicle); // calcula ocupacion y asigna dia siguiente si es necesario
+
+        }
+
+        private int chechkTypeVehicle(string typeVehicle) 
+        {
+            if (typeVehicle == "renault")
+            {
+                return 1;
+            }
+            else 
+            {
+                if (typeVehicle == "chevrolet")
+                {
+                    return 2;
+                }
+                else 
+                {
+                    if (typeVehicle == "ford")
+                    {
+                        return 3;
+                    }
+                    else 
+                    {
+                        if (typeVehicle == "toyota")
+                        {
+                            return 4;
+                        }
+                    }
+                }
+            }
+            return 0;
+
+        }
+
+        private void avalibleDay(int occupation, string day, int checkType)
+        {
+            int high = 0;            
+
+            while (occupation <= 84)
+            {
+                high++;
+
+                if (day == "sabado" && high * checkType > 4)
+                {
+                    quantityAvalible = --high;
+                    assignedDay = checkDay(day);
+                    //_checkQuantity = checkQuantity(_quantity, quantityAvalible, _checkTypeVehicle);
+                    high = 0;
+                    occupation = 90;
+                }
+                else 
+                {
+                    if (high * checkType > 16)
+                    {
+                        quantityAvalible = --high;
+                        assignedDay = checkDay(day);
+                        //_checkQuantity = checkQuantity(_quantity, quantityAvalible, _checkTypeVehicle);
+                        high = 0;
+                        occupation = 90;
+                    }
+                }
+            }    
+        }
+
+        private string checkDay(string day) 
+        {
+            string[] dayCheck = new string[6];
+
+            dayCheck[0] = "lunes";
+            dayCheck[1] = "martes";
+            dayCheck[2] = "miercoles";
+            dayCheck[3] = "jueves";
+            dayCheck[4] = "viernes";
+            dayCheck[5] = "sabado";
+
+            for (int i = 0; i < dayCheck.Length; i++)
+            {
+                if (day == dayCheck[i]) 
+                {
+                    if (i == 5) 
+                    {
+                        return dayCheck[0];
+                    }
+
+                    return dayCheck[i + 1];
+                }
+            }
+            return "";
+        }
+
+
+        private int checkQuantity(int quantity, int avalibleQuantity, int checkTypeVehicle)
+        {
+            if (checkTypeVehicle * quantity >= 16) 
+            {
+                return avalibleQuantity * checkTypeVehicle ;
+            }
+            return 0;
+        }
+    }
+}
